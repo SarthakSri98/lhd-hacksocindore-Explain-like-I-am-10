83f6d9d4ec4016882514b309ef4e2b28847f959b
SarthakSri98 <sarthaksri1998@gmail.com>
2018-12-09 14:12:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule,Routes } from '@angular/router'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from "@angular/forms";
+
+const route : Routes = [
+  { path:'home', component:HomeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,MatToolbarModule,MatInputModule,MatDialogModule,MatButtonModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog,MatDialogConfig,MAT_DIALOG_DATA,MatTooltip } from '@angular/material/';
+import { transition,trigger,style,animate,state,stagger,query, keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    
+    trigger('fadeIn',  [
+       transition('*=>*',[
+          query(':enter',style({opacity:0,transform:'translate(0,-20px)'}),{optional:true}),
+          query(':enter',stagger('400ms',[
+               animate('.3s ease-in',style({ opacity:1,transform:'translate(0,0)' }))
+  
+          ]),{optional:true}),
+        ])
+      ])
+    ]
+})
+export class HomeComponent implements OnInit {
+
+  topicArray = [
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+    { title:"Cloud Computing" , description: "Cloud computing means storing and accessing data and programs over the Internet instead of your computer's hard drive" },
+  ]
+  title;
+  description;
+  height = '500px';
+  width= '600px';
+  constructor(private modalService: MatDialog ) { }
+
+  ngOnInit() {
+  }
+
+
+  open(content,i) {
+
+    this.title = this.topicArray[i].title;
+    this.description = this.topicArray[i].description;
+
+    const dialogConfig = new MatDialogConfig();
+    this.modalService.open(content,{
+      height: this.height,
+      width: this.width,
+      panelClass: 'custom-modalbox'
+    });
+
+    
+  }
+  addNew(content) {
+
+    const dialogConfig = new MatDialogConfig();
+    this.modalService.open(content,{
+      height: this.height,
+      width: this.width,
+      panelClass: 'custom-modalbox'
+    });
+
+    
+  }
+  Cross_click(){
+      this.modalService.closeAll();
+  }
+
+  
+}
